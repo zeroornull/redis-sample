a070909064a503a4c73a5c32c55a917b2c6f61af
zeroornull <xxpfantastic@yahoo.com>
2024-12-25 21:27:06 +0800

    chore: project inti.

---
+++ src/test/java/io/github/RedisSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
